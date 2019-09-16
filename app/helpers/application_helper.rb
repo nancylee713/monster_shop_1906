78fbd8042227e9fc794bdec27ae32b15b19ac9ce
Nancy Lee <srlee713@gmail.com>
2019-09-16 12:43:23 -0600

    Add bootstrap styling to flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def bootstrap_class_for flash_type
+    case flash_type
+      when "success"
+        "alert-success"
+      when "error"
+        "alert-danger"
+      when "limit"
+        "alert-danger"
+      when "incomplete_order"
+        "alert-danger"
+      when "incomplete_merchant"
+        "alert-danger"
+      when "notice"
+        "alert-info"
+      when "delete_item_warning"
+        "alert-danger"
+      when "no_address"
+        "alert-danger"
+      when "delete_warning"
+        "alert-danger"
+      when "inactive_item"
+        "alert-danger"
+    end
+  end
 end
