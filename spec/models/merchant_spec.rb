1546082eccc576334270ce6f7d4e8844b020ca11
Nancy Lee <srlee713@gmail.com>
2019-09-15 19:54:32 -0600

    Update #distinct_cities method in merchant model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -17,6 +17,7 @@ describe Merchant, type: :model do
   describe 'instance methods' do
     before(:each) do
       @user = create(:user)
+      @address = create(:address)
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
     end
@@ -24,7 +25,7 @@ describe Merchant, type: :model do
     it 'no_orders' do
       expect(@meg.no_orders?).to eq(true)
 
-      order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      order_1 = create(:order, user: @user, address: @address)
       item_order_1 = @user.item_orders.create!(order: order_1, item: @tire, price: @tire.price, quantity: 2)
 
       expect(@meg.no_orders?).to eq(false)
@@ -44,9 +45,10 @@ describe Merchant, type: :model do
 
     it 'distinct_cities' do
       chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 40, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 22)
-      order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
-      order_2 = Order.create!(name: 'Brian', address: '123 Brian Ave', city: 'Denver', state: 'CO', zip: 17033)
-      order_3 = Order.create!(name: 'Dao', address: '123 Mike Ave', city: 'Denver', state: 'CO', zip: 17033)
+      order_1 = create(:order, user: @user, address: create(:address, city: "Denver"))
+      order_2 = create(:order, user: @user, address: create(:address, city: "Denver"))
+      order_3 = create(:order, user: @user, address: create(:address, city: "Hershey"))
+
       @user.item_orders.create!(order: order_1, item: @tire, price: @tire.price, quantity: 2)
       @user.item_orders.create!(order: order_2, item: chain, price: chain.price, quantity: 2)
       @user.item_orders.create!(order: order_3, item: @tire, price: @tire.price, quantity: 2)
@@ -77,7 +79,7 @@ describe Merchant, type: :model do
     end
 
     it "pending_orders" do
-      order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      order_1 = create(:order, user: @user, address: @address)
       item_order_1 = @user.item_orders.create!(order: order_1, item: @tire, price: @tire.price, quantity: 2)
       expect(@meg.pending_orders).to eq([order_1])
     end
