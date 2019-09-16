9c4ab03b4f8d564d465441d36536f7572ffb293a
Nancy Lee <srlee713@gmail.com>
2019-09-15 18:07:34 -0600

    Add additional validations to address model attributes

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,8 @@
 class Address < ApplicationRecord
   validates_presence_of :street, :city, :state, :zipcode, :nickname
 
+  validates :state, length: { is: 2 }
+  validates :zipcode, length: { is: 5 }, numericality: { only_integer: true }
+
   belongs_to :user
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -5,7 +5,10 @@ describe Address, type: :model do
     it { should validate_presence_of :street }
     it { should validate_presence_of :city }
     it { should validate_presence_of :state }
+    it { should validate_length_of(:state).is_equal_to(2) }
     it { should validate_presence_of :zipcode }
+    it { should validate_numericality_of(:zipcode).only_integer }
+    it { should validate_length_of(:zipcode).is_equal_to(5) }
     it { should validate_presence_of :nickname }
   end
 
