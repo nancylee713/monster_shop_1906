bdc8f65b89b5bdf023dd568e84a2aacfb1dfe59b
Nancy Lee <srlee713@gmail.com>
2019-09-16 23:38:08 -0600

    Add create functionality to coupon model

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -8,4 +8,25 @@ class Merchant::CouponsController < Merchant::BaseController
   def index
     @coupons = @merchant.coupons
   end
+
+  def new
+    @coupon = @merchant.coupons.new
+  end
+
+  def create
+    @coupon = @merchant.coupons.create(coupon_params)
+    if @coupon.save
+      flash[:new_coupon] = "New coupon is created!"
+      redirect_to merchant_coupons_path
+    else
+      flash[:error] = @coupon.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def coupon_params
+    params.require(:coupon).permit(:name, :value, :is_percent, :item_id)
+  end
 end
---
+++ spec/features/coupons/new_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe "Coupon creation" do
+  describe "As Merchant Admin" do
+    describe "My coupon index page" do
+      before(:each) do
+        @bike_shop = create(:merchant)
+
+        @tire = create(:item, merchant: @bike_shop)
+        @chain = create(:item, merchant: @bike_shop)
+        @merchant_admin = create(:merchant_admin, merchant: @bike_shop)
+
+        visit login_path
+        fill_in "Email", with: @merchant_admin.email
+        fill_in "Password", with: @merchant_admin.password
+        click_on "Submit"
+      end
+
+      it "I can click a link to add new coupon" do
+        visit merchant_coupons_path
+        click_link("Add New Coupon")
+
+        expect(current_path).to eq(new_merchant_coupon_path)
+
+        fill_in "Value", with: 15
+        find(:css, "#coupon_is_percent").set(false)
+        find("option[value=#{@tire.id}]").click
+        click_on "Create Coupon"
+
+        expect(current_path).to eq(merchant_coupons_path)
+
+        new_coupon = Coupon.last
+
+        within "#coupon-#{new_coupon.id}" do
+          expect(page).to have_content(new_coupon.id)
+          expect(page).to have_content(new_coupon.name)
+          expect(page).to have_content("$#{new_coupon.value.to_f}")
+          expect(page).to have_content(@tire.name)
+          expect(page).to have_content(new_coupon.is_enabled)
+        end
+      end
+    end
+  end
+end
